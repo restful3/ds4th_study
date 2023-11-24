647211585b13361b2d4be9a6d9f9ec7ae0624765
restful3 <restful3@gmai.com>
2023-11-25 07:09:15 +0900

    바닥 2

---
+++ source/밑바닥부터_시작하는_딥러닝_2/DLFromScratch2/README.md
@@ -0,0 +1,26 @@
+# Deep Learning from Scratch ❷
+
+## 1. 교재
+
+![](./cover.jpg)
+
+
+
+## 2. 목차
+
+- 1장 - [신경망 복습](<https://github.com/ExcelsiorCJH/DLFromScratch2/blob/master/Chap01-Neural_Networks_Review/notebook.ipynb>)
+- 2장 - [자연어와 단어의 분산 표현](<https://github.com/ExcelsiorCJH/DLFromScratch2/blob/master/Chap02-Natural_Language_and_Distributed_Representation/notebook.ipynb>)
+- 3장 - [Word2Vec](<https://github.com/ExcelsiorCJH/DLFromScratch2/blob/master/Chap03-Word2Vec/notebook.ipynb>)
+- 4장 - [Word2Vec 속도 개선](<https://github.com/ExcelsiorCJH/DLFromScratch2/blob/master/Chap04-Word2Vec_Improved/notebook.ipynb>)
+- 5장 - [순환 신경망(RNN)](<https://github.com/ExcelsiorCJH/DLFromScratch2/blob/master/Chap05-Recurrent_Neural_Network/notebook.ipynb>)
+- 6장 - [게이트가 추가된 RNN](https://nbviewer.jupyter.org/github/ExcelsiorCJH/DLFromScratch2/blob/master/Chap06-Fancy_RNN/notebook.ipynb)
+- 7장 - [RNN을 사용한 문장 생성](https://nbviewer.jupyter.org/github/ExcelsiorCJH/DLFromScratch2/blob/master/Chap07-Seq2Seq/notebook.ipynb)
+- 8장 - [어텐션(Attention)](https://nbviewer.jupyter.org/github/ExcelsiorCJH/DLFromScratch2/blob/master/Chap08-Attention/notebook.ipynb)
+
+
+
+## 3. 참고자료
+
+- **GitHub** :  <https://github.com/WegraLee/deep-learning-from-scratch-2>
+
+- **errata** : <https://docs.google.com/document/d/1pzeh5nrP6y6A5WgT9vvxMpe-ai7ZRhU84BdAhdJzuFk/edit?usp=sharing>
---
+++ source/밑바닥부터_시작하는_딥러닝_2/common/util.py
@@ -0,0 +1,293 @@
+import sys
+sys.path.append('..')
+import os
+from common.np import *
+
+
+def preprocess(text):
+    text = text.lower()
+    text = text.replace('.', ' .')
+    words = text.split(' ')
+
+    word_to_id = {}
+    for word in words:
+        if word not in word_to_id:
+            new_id =  len(word_to_id)
+            word_to_id[word] = new_id
+            
+    id_to_word = {id_: word for word, id_ in word_to_id.items()}
+    
+    corpus = np.array([word_to_id[word] for word in words])
+    return corpus, word_to_id, id_to_word
+
+
+def cos_similarity(x, y, eps=1e-8):
+    '''코사인 유사도 산출
+    :param x: 벡터
+    :param y: 벡터
+    :param eps: '0으로 나누기'를 방지하기 위한 작은 값
+    :return:
+    '''
+    nx = x / np.sqrt(np.sum(x**2) + eps)  # x의 정규화
+    ny = y / np.sqrt(np.sum(y**2) + eps)  # y의 정규화
+    return np.dot(nx, ny)
+
+
+def most_similar(query, word_to_id, id_to_word, word_matrix, top=5):
+    '''유사 단어 검색
+    :param query: 쿼리(텍스트)
+    :param word_to_id: 단어에서 단어 ID로 변환하는 딕셔너리
+    :param id_to_word: 단어 ID에서 단어로 변환하는 딕셔너리
+    :param word_matrix: 단어 벡터를 정리한 행렬. 각 행에 해당 단어 벡터가 저장되어 있다고 가정한다.
+    :param top: 상위 몇 개까지 출력할 지 지정
+    '''
+    # 1) 검색어를 꺼낸다.
+    if query not in word_to_id:
+        print(f'{query}(을)를 찾을 수 없습니다.')
+        return 
+    
+    print(f'\n[query] {query}')
+    query_id = word_to_id[query]
+    query_vec = word_matrix[query_id]
+    
+    # 2) 코사인 유사도 계산
+    vocab_size = len(id_to_word)
+    similarity = np.zeros(vocab_size)
+    for i in range(vocab_size):
+        similarity[i] = cos_similarity(word_matrix[i], query_vec)
+        
+    # 3) 코사인 유사도를 기준으로 내림차순으로 출력
+    count = 0
+    for i in (-1 * similarity).argsort():
+        if id_to_word[i] == query:
+            continue
+        print(f' {id_to_word[i]}: {similarity[i]}')
+        
+        count +=1
+        if count >= top:
+            return
+
+
+def convert_one_hot(corpus, vocab_size):
+    '''원핫 표현으로 변환
+    :param corpus: 단어 ID 목록(1차원 또는 2차원 넘파이 배열)
+    :param vocab_size: 어휘 수
+    :return: 원핫 표현(2차원 또는 3차원 넘파이 배열)
+    '''
+    N = corpus.shape[0]
+    if corpus.ndim == 1:
+        one_hot = np.zeros((N, vocab_size), dtype=np.int32)
+        for idx, word_id in enumerate(corpus):
+            one_hot[idx, word_id] = 1
+            
+    elif corpus.ndim == 2:
+        C = corpus.shape[1]
+        one_hot = np.zeros((N, C, vocab_size), dtype=np.int32)
+        for idx_0, word_ids in enumerate(corpus):
+            for idx_1, word_id in enumerate(word_ids):
+                one_hot[idx_0, idx_1, word_id] = 1
+                
+    return one_hot
+
+
+def create_co_matrix(corpus, vocab_size, window_size=1):
+    '''동시발생 행렬 생성
+    :param corpus: 말뭉치(단어 ID 목록)
+    :param vocab_size: 단어 수
+    :param window_size: 윈도우 크기(윈도우 크기가 1이면 타깃 단어 좌우 한 단어씩이 맥락에 포함)
+    :return: 동시발생 행렬
+    '''
+    corpus_size = len(corpus)
+    co_matrix = np.zeros((vocab_size, vocab_size), dtype=np.int32)
+    
+    for idx, word_id in enumerate(corpus):
+        for i in range(1, window_size + 1):
+            left_idx = idx - i  # left window_size
+            right_idx = idx + i  # right window_size
+
+            if left_idx >= 0:
+                left_word_id = corpus[left_idx]
+                co_matrix[word_id, left_word_id] += 1
+
+            if right_idx < corpus_size:
+                right_word_id = corpus[right_idx]
+                co_matrix[word_id, right_word_id] += 1
+                
+    return co_matrix
+
+
+# common/util.py
+def ppmi(C, verbose=False, eps=1e-8):
+    '''PPMI(점별 상호정보량) 생성
+    :param C: 동시발생 행렬
+    :param verbose: 진행 상황을 출력할지 여부
+    :return: ppmi
+    '''
+    M = np.zeros_like(C, dtype=np.float32)
+    N = np.sum(C)  # num of corpus
+    S = np.sum(C, axis=0)  # 각 단어의 출현 횟수
+    total = C.shape[0] * C.shape[1]
+    cnt = 0
+    
+    for i in range(C.shape[0]):
+        for j in range(C.shape[1]):
+            pmi = np.log2(C[i, j] * N / (S[i]*S[j]) + eps)
+            M[i, j] = max(0, pmi)
+            
+            if verbose:
+                cnt += 1
+                if cnt % (total//100) == 0:
+                    print(f'{(100*cnt/total):.2f} 완료')
+    return M
+
+
+def eval_perplexity(model, corpus, batch_size=10, time_size=35):
+    print('퍼플렉서티 평가 중 ...')
+    corpus_size = len(corpus)
+    total_loss, loss_cnt = 0, 0
+    max_iters = (corpus_size - 1) // (batch_size * time_size)
+    jump = (corpus_size - 1) // batch_size
+
+    for iters in range(max_iters):
+        xs = np.zeros((batch_size, time_size), dtype=np.int32)
+        ts = np.zeros((batch_size, time_size), dtype=np.int32)
+        time_offset = iters * time_size
+        offsets = [time_offset + (i * jump) for i in range(batch_size)]
+        for t in range(time_size):
+            for i, offset in enumerate(offsets):
+                xs[i, t] = corpus[(offset + t) % corpus_size]
+                ts[i, t] = corpus[(offset + t + 1) % corpus_size]
+
+        try:
+            loss = model.forward(xs, ts, train_flg=False)
+        except TypeError:
+            loss = model.forward(xs, ts)
+        total_loss += loss
+
+        sys.stdout.write('\r%d / %d' % (iters, max_iters))
+        sys.stdout.flush()
+
+    print('')
+    ppl = np.exp(total_loss / max_iters)
+    return ppl
+
+def eval_seq2seq(model, question, correct, id_to_char,
+                 verbos=False, is_reverse=False):
+    correct = correct.flatten()
+    # 머릿글자
+    start_id = correct[0]
+    correct = correct[1:]
+    guess = model.generate(question, start_id, len(correct))
+
+    # 문자열로 변환
+    question = ''.join([id_to_char[int(c)] for c in question.flatten()])
+    correct = ''.join([id_to_char[int(c)] for c in correct])
+    guess = ''.join([id_to_char[int(c)] for c in guess])
+
+    if verbos:
+        if is_reverse:
+            question = question[::-1]
+
+        colors = {'ok': '\033[92m', 'fail': '\033[91m', 'close': '\033[0m'}
+        print('Q', question)
+        print('T', correct)
+
+        is_windows = os.name == 'nt'
+
+        if correct == guess:
+            mark = colors['ok'] + '☑' + colors['close']
+            if is_windows:
+                mark = 'O'
+            print(mark + ' ' + guess)
+        else:
+            mark = colors['fail'] + '☒' + colors['close']
+            if is_windows:
+                mark = 'X'
+            print(mark + ' ' + guess)
+        print('---')
+
+    return 1 if guess == correct else 0  
+
+def create_contexts_target(corpus, window_size=1):
+    '''맥락과 타깃 생성
+    :param corpus: 말뭉치(단어 ID 목록)
+    :param window_size: 윈도우 크기(윈도우 크기가 1이면 타깃 단어 좌우 한 단어씩이 맥락에 포함)
+    :return: (맥락, 타겟)의 np.array
+    '''
+    target = corpus[window_size:-window_size]
+    contexts = []
+    
+    for idx in range(window_size, len(corpus)-window_size):
+        cs = []
+        # wiondow_size만큼 타겟 단어 좌우 context 가져오기
+        for t in range(-window_size, window_size+1):
+            if t != 0:
+                cs.append(corpus[idx + t])
+        contexts.append(cs)
+        
+    return np.array(contexts), np.array(target)
+
+
+def to_cpu(x):
+    import numpy
+    if type(x) == numpy.ndarray:
+        return x
+    return np.asnumpy(x)
+
+
+def to_gpu(x):
+    import cupy
+    if type(x) == cupy.ndarray:
+        return x
+    return cupy.asarray(x)
+    
+
+def clip_grads(grads, max_norm):
+    total_norm = 0
+    for grad in grads:
+        total_norm += np.sum(grad ** 2)
+    total_norm = np.sqrt(total_norm)
+
+    rate = max_norm / (total_norm + 1e-6)
+    if rate < 1:
+        for grad in grads:
+            grad *= rate
+
+
+def analogy(a, b, c, word_to_id, id_to_word, word_matrix, top=5, answer=None):
+    for word in (a, b, c):
+        if word not in word_to_id:
+            print(f'{word}(을)를 찾을 수 없습니다.')
+            return
+    
+    print(f'\n[analogy] {a}:{b} = {c}:?')
+    a_vec, b_vec, c_vec = word_matrix[word_to_id[a]], word_matrix[word_to_id[b]], word_matrix[word_to_id[c]]
+    query_vec = b_vec - a_vec + c_vec
+    query_vec = normalize(query_vec)
+
+    similarity = np.dot(word_matrix, query_vec)
+
+    if answer is not None:
+        print(f"==>{answer}:{str(np.dot(word_matrix[word_to_id[answer]], query_vec))}")
+    
+    count = 0
+    for i in (-1 * similarity).argsort():
+        if np.isnan(similarity[i]):
+            continue
+        if id_to_word[i] in (a, b, c):
+            continue
+        print(' {0}: {1}'.format(id_to_word[i], similarity[i]))
+
+        count += 1
+        if count >= top:
+            return
+
+
+def normalize(x):
+    if x.ndim == 2:
+        s = np.sqrt((x * x).sum(1))
+        x /= s.reshape((s.shape[0], 1))
+    elif x.ndim == 1:
+        s = np.sqrt((x * x).sum())
+        x /= s
+    return x
