8061183ab6a68f7a64d3c6988e800222fa192fc8
kimxx606 <127455748+kimxx606@users.noreply.github.com>
2024-06-15 12:20:25 +0900

    Add files via upload

---
+++ source/랭체인_완벽_입문/chainlit_callbacks_code.py
@@ -0,0 +1,34 @@
+import chainlit as cl
+from langchain.agents import AgentType, initialize_agent, load_tools
+from langchain.chat_models import ChatOpenAI
+
+chat = ChatOpenAI( 
+    temperature=0,
+    # model="gpt-3.5-turbo",
+    model="gpt-4o",
+    api_key = api_key
+)
+
+tools=load_tools(
+    [
+        "serpapi"        
+    ],
+    serpapi_api_key =serpapi_api_key
+    # llm=chat
+)
+
+agent=initialize_agent(tools=tools, llm=chat, agent=AgentType.CHAT_ZERO_SHOT_REACT_DESCRIPTION, verbose=True)
+
+@cl.on_chat_start
+async def on_chat_start():
+    await cl.Message(content="Agent 초기화 완료").send()
+    
+@cl.on_message
+async def on_message(input_message):
+    result=agent.run(
+        input_message.content,
+        callbacks=[
+            cl.LangchainCallbackHandler()
+        ]
+    )
+    await cl.Message(content=result).send()
